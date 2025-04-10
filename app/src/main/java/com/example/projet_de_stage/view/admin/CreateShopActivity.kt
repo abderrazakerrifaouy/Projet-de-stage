5bd734af9196366d9bb2003547237372f44c2c5a
abnderrazak-errifaouy <abdrzakrifawi@gmail.com>
2025-04-10 14:30:18 +0100

    create activity CreateShopActivity.kt

---
+++ app/src/main/java/com/example/projet_de_stage/view/admin/CreateShopActivity.kt
@@ -0,0 +1,12 @@
+package com.example.projet_de_stage.view.admin
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.projet_de_stage.R
+
+class CreateShopActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_shop)
+    }
+}
